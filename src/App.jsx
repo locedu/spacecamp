180bb5076a13e72462c99e526fbc1161d720185d
locedu <locutus.edu@outlook.com>
2025-03-02 12:09:20 -0500

    route view post (/dashboard/posts/:id)

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import TokenValidation from './components/TokenValidation';  
+import TokenValidation from './components/TokenValidation';
 
 import Dashboard from './views/Dashboard';
-import Feed from './views/Feed';
 import Profile from './views/Profile';
 import Notifications from './views/Notifications';
 import Friends from './views/Friends';
 import Activity from './views/Activity';
-import Home from './views/Home'; 
+import Home from './views/Home';
 import Login from './views/Login';
 import Register from './views/Register';
-import AllSections from './views/AllSections'; 
+import AllSections from './views/AllSections';
 import PrivateRoute from './components/PrivateRoute';
 import Navbar from './components/Navbar';
 
-import NewPost from './views/NewPost';  // New Post
-import EditPost from './views/EditPost'; // Edit Post
-import ViewPost from './views/ViewPost'; // View Post
+import Posts from './views/Feed'; // Renaming Feed to Posts for clarity
+import NewPost from './views/NewPost';
+import EditPost from './views/EditPost';
+import ViewPost from './views/ViewPost';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./styles/app.css";
 
 function App() {
-  const token = useSelector((state) => state.auth.token); 
+  const token = useSelector((state) => state.auth.token);
 
   return (
     <Router>
-      <TokenValidation token={token} /> 
+      <TokenValidation token={token} />
 
       <Navbar />
       <main className="content">
@@ -42,7 +42,7 @@ function App() {
           <Route path="/dashboard" element={<PrivateRoute element={<Dashboard />} />}>
             <Route index element={<AllSections />} />
             <Route path="profile" element={<Profile />} />
-            <Route path="feed" element={<Feed />} />
+            <Route path="posts" element={<Posts />} /> {/* Updated Route */}
             <Route path="notifications" element={<Notifications />} />
             <Route path="friends" element={<Friends />} />
             <Route path="activity" element={<Activity />} />
