a52ac472ba1706a78561cf137d83ab2f3dae21c3
locedu <locutus.edu@outlook.com>
2025-03-15 15:41:24 -0400

    vite config for render

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Allows access from external sources during development
+    port: 5173, // Ensure the correct port is used
+  },
+  preview: {
+    allowedHosts: ["spacebar-react.onrender.com"], // ✅ Add your Render domain here
+  },
 })
